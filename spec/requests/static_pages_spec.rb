b2d0046d87214ae02662bc37c4313170b082c3e3
Walter Gray Somerville <waltergraysomerville@gmail.com>
2013-08-14 20:06:02 -0400

    static pages, next up slightly dynamic pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,11 +11,19 @@ describe "Static pages" do
   end
 
   describe "Help page" do
-  	
+
 
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+  end
+
+  describe "About page" do
+
+  	it "should have the content 'About'" do
+  		visit '/static_pages/about'
+  		expect(page).to have_content('About')
+  	end
   end
 end
